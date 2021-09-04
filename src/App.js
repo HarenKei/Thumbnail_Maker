a5a50a886caa7d3c434d949d773ec5e18e2c7c4b
Seungmin Jeong <seungmin1266@gmail.com>
2021-09-04 18:50:28 +0900

    Update App.js : input text test code

--- src/App.js
+++ src/App.js
@@ -4,13 +4,19 @@ import './App.css';
 const Thumbnail = () =>{
   const [image, setImage] = useState(null)
   const canvas = useRef(null)
+  const [thumbText, setThumbText] = useState(null)
   
   useEffect(()=>{
     const bgImage = new Image();
-    bgImage.src = 
+    bgImage.src = "./image/background.jpeg"
     bgImage.onload = () => setImage(bgImage)
   }, [])
 
+  const onChange = (event) => {
+    /*  event: 사용자의 액션에 따라서 자동으로 이벤트가 
+    생성되며, 특정한 파라미터가 없을경우, 기본 파라미터로 이벤트 객체가 생성된다. */
+      setThumbText(event.target.value);
+  }
 
 
 return (
@@ -18,9 +24,11 @@ return (
     <h1>STARGAZER blog Thumbnail Maker!</h1>
 
     <div>
-      <canvas ref={ canvas } width="800px" height="500px">
-
-      </canvas>
+      <canvas ref={ canvas } width="800px" height="500px" /> {/*썸네일의 배경이 될 이미지를 표시하며, 텍스트 작성을 보여준다.*/}
+    </div>
+    <div>
+      썸네일 텍스트 : <input type="text" value={ thumbText } onChange={onChange} />
+      { thumbText }
     </div>
   </div>
 )
