feb708101a8440c3ed4344fda704de0b8cccd980
Seungmin Jeong <seungmin1266@gmail.com>
2021-09-05 22:55:15 +0900

    Update App.js : Add button function for Color Picker

--- src/App.js
+++ src/App.js
@@ -5,27 +5,25 @@ import './App.css';
 
 const Thumbnail = () =>{
   const [color, setColor] = useState('#fff')
+  const [showButton, setShowButton] = useState(false)
   const canvas = useRef(null)
   const [thumbText, setThumbText] = useState('')
 
   useEffect(() => {
       const ctx = canvas.current.getContext("2d")
-      ctx.fillStyle="#327da8"
+      ctx.fillStyle= {color}
       ctx.fillRect(0, 0, 960, 540)
 
       ctx.font = "50px Comic Sans MS"
-      ctx.fillStyle = { color }
+      ctx.fillStyle = "white"
       ctx.fillText(thumbText, (800/2), (500/2))
       ctx.textBaseline = "middle"
       ctx.textAlign = "center"
 
-  }, [canvas, thumbText])
+  }, [canvas, thumbText, color])
 
 
-  const fikColor = (color) =>{
-    setColor({ color : color.hex })
-  }
-
+  
 return (
   <div>
     <h1>STARGAZER blog Thumbnail Maker</h1>
@@ -38,6 +36,18 @@ return (
       /> 
     
     </div>
+
+    <div>
+      <button onClick={()=>setShowButton(showButton=>!showButton)}>
+        {showButton ? '배경 색상 선택창 닫기' : '배경 색상 선택하기'}
+      </button>
+      {showButton && (<BlockPicker
+                        color={color}
+                        onChange={color => setColor(color.hex)}/>
+                      )
+      }
+
+    </div>
    
     <div>
       썸네일 텍스트 : 
@@ -46,9 +56,7 @@ return (
         onChange = {e=> setThumbText(e.target.value)} 
       />
 
-      <div>
-        <BlockPicker color={color} onChange={fikColor} />
-      </div>
+      
       
     </div>
   </div>
