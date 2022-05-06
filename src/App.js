ebc603c0e9a97713cfcd0952007625ba57d4efc3
harenkei <seungmin1266@gmail.com>
2022-05-06 14:03:33 +0900

    [Updated App.js / App.css] Add some CSS styles

--- src/App.js
+++ src/App.js
@@ -9,13 +9,13 @@ import './App.css';
 
 
 const App = () =>{
-  const [color, setColor] = useState('#dae5e8');
-  const [showButton, setShowButton] = useState(false);
-  const [showTxtButton, setShowTxtButton] = useState(false);
-  const canvas = useRef(null);
-  const [thumbText, setThumbText] = useState('');
-  const [cnvsWidth, setCnvsWidth] = useState(800);
-  const [cnvsHeight, setCnvsHeight] = useState(500);
+  const [color, setColor] = useState('#dae5e8'); //canvas color
+  const [showButton, setShowButton] = useState(false); // canvas color
+  const [showTxtButton, setShowTxtButton] = useState(false); // text color
+  const canvas = useRef(null);// canvas 
+  const [thumbText, setThumbText] = useState(''); //canvas text
+  const [cnvsWidth, setCnvsWidth] = useState(800); //canvas width
+  const [cnvsHeight, setCnvsHeight] = useState(540);
   const [textSize, setTextSize] = useState("80px");
   const [textColor, setTextColor] = useState('#ffffff'); 
   const options = [ 
@@ -26,20 +26,32 @@ const App = () =>{
     { label: "120px", value: "120px" },
     { label: "150px", value: "150px" }
   ];
-  
-
+  const widthOptions = [
+    {label : "640px", value : 640},
+    {label : "800px", value : 800},
+    {label : "864px", value : 864},
+    {label : "960px", value : 960},
+  ];
+  const heightOptions = [
+    {label : "360px", value : 360},
+    {label : "450px", value : 450},
+    {label : "486px", value : 486},
+    {label : "540px", value : 540},
+  ];
   var thumbName ='';
 
   useEffect(() => { 
+      var width = cnvsWidth.value;
+      var height = cnvsHeight.value;
       
       const ctx = canvas.current.getContext("2d")
       //Start of canvas useEffect
       ctx.fillStyle = color
-      ctx.fillRect(0, 0, cnvsWidth, cnvsHeight)
-      ctx.font = textSize + " Noto Sans CJK KR"
+      ctx.fillRect(0, 0, 800, 500)
+      ctx.font = textSize.value + " Noto Sans CJK KR"
       console.log(ctx.font)
       ctx.fillStyle = textColor 
-      ctx.fillText(thumbText, (cnvsWidth/2), (cnvsHeight/2))
+      ctx.fillText(thumbText, 800/2, 500/2)
       ctx.textBaseline = "middle"
       ctx.textAlign = "center"
       //End of canvas useEffect
@@ -76,57 +88,37 @@ return (
 
     <div id = "divCanvasAdjustment"> 
 
+      
+
+      
+    <div id = "divTextAndSize">
+      <div id="divTextInput">
+          <input type = "text" placeholder = "썸네일 텍스트" 
+            value = { thumbText } 
+          onChange = { e=> setThumbText(e.target.value) } 
+         />
+        </div> { /* End of divTextInput */ }
+
+        <div id = "divThumbnailSave">
+          <button className = 'thumbDown' onClick = { onDownloadBtn }>
+          썸네일 저장
+          </button>
+        </div> {/* End of divThumbnailSave */}
+    </div>{/* End of divTextAndSize */}
+
+    <div id = "divOterAdjustment">
       <div id = "divColorpicker">
-        <button onClick = {()=>setShowButton(showButton=>!showButton)}>
-          {showButton ? '배경 색상 선택창 닫기' : '배경 색상 선택하기'}
-        </button>
-        {showButton && (<SketchPicker
-                          color={ color }
-                          onChangeComplete={ (color) => setColor(color.hex) }/>
-                        )
-        }
+          <button onClick = {()=>setShowButton(showButton=>!showButton)}>
+            {showButton ? '배경 색상 선택창 닫기' : '배경 색상 선택하기'}
+         </button>
+         {showButton && (<SketchPicker
+                           color={ color }
+                           onChangeComplete={ (color) => setColor(color.hex) }/>
+                         )
+           }
       </div> {/* End of divColorpicker */}
 
-      <div id = "divThumbnailSave">
-        <button className = 'thumbDown' onClick = { onDownloadBtn }>
-        썸네일 저장
-        </button>
-      </div> {/* End of divThumbnailSave */}
-   
-     <div id="divTextInput">
-        <input type = "text" placeholder = "썸네일 텍스트" 
-          value = { thumbText } 
-          onChange = { e=> setThumbText(e.target.value) } 
-        />
-      </div> { /* End of divTextInput */ }
-
-    <div id = "divCnavsWidth">
-      width : 
-      <input type = "text" placeholder = "width"
-        value = { cnvsWidth }
-        onChange = { e => setCnvsWidth(e.target.value) }
-      />
-    </div>
-
-    <div id = "divCnavsHeight">
-      height : 
-      <input type = "text" placeholder = "height"
-        value = { cnvsHeight }
-        onChange = { e => setCnvsHeight(e.target.value) }
-      />
-    </div>
-
-    <div id = "divSelectSize">
-      <Select 
-      className = "fontSize" 
-      placeholder = "폰트 사이즈"
-      options = { options } 
-      value = { textSize }
-      onChange = { e => setTextSize(e) }
-      />
-    </div>{/* End of divSelectSize*/}
-
-    <div id = "divTxtColorpicker">
+      <div id = "divTxtColorpicker">
         <button onClick = {()=>setShowTxtButton(showTxtButton=>!showTxtButton)}>
           { showTxtButton ? '폰트 색상 선택창 닫기' : '폰트 색상 선택하기' }
         </button>
@@ -137,6 +129,27 @@ return (
         }
       </div>
 
+      <div id = "divCnavsSize">
+        <Select 
+           className = "canvasWidHei" 
+           placeholder = "Width * Height"
+           options = { widthOptions } 
+           value = { cnvsWidth }
+           onChange = { e => setCnvsWidth(e) }
+        />
+      </div>
+
+      <div id = "divSelectSize">
+      <Select 
+      className = "fontSize" 
+      placeholder = "폰트 사이즈"
+      options = { options } 
+      value = { textSize }
+      onChange = { e => setTextSize(e) }
+      />
+    </div>{/* End of divSelectSize*/}
+     </div>
+   
 
     </div> {/* End of divCanvasAdjustment */}
 
